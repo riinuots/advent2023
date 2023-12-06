bdd92e3386f84bdaf3b3778a71e6d3440153324b
riinuots <riinu.pius@ed.ac.uk>
2023-12-06 12:54:30 +0000

    now with Part II

--- solutions/day03/gear_ratios.R
+++ solutions/day03/gear_ratios.R
@@ -2,7 +2,6 @@ library(tidyverse)
 
 input_orig = read_csv("solutions/day03/input", col_names = "part")
 
-# Part I only
 input = input_orig %>% 
   rowid_to_column("x") %>% 
   separate_rows(part, sep = "") %>% 
@@ -21,13 +20,17 @@ groups = input %>%
 
 neigbhours = crossing(dy = -1:1, dx = -1:1)
 
-keep = input %>% 
+engine = input %>% 
   crossing(neigbhours) %>% 
   mutate(nx = x+dx,
          ny = y+dy) %>% 
   left_join(rename(input, nb = part), by = c("nx" = "x", "ny" = "y")) %>% 
   drop_na() %>% 
-  filter(nb != ".") %>% 
+  filter(nb != ".")
+
+# Part I
+
+keep = engine %>% 
   filter(!str_detect(nb, "\\d")) %>% 
   select(x, y) %>% 
   mutate(id = paste(x, y, sep = "-"))
@@ -42,3 +45,24 @@ groups %>%
   summarise(.by = group_id,
             part = paste(part, collapse = "") %>% parse_number()) %>% 
   summarise(sum(part))
+
+# Part II
+gears = engine %>% 
+  filter(part == "*") %>% 
+  filter(str_detect(nb, "\\d")) %>% 
+  mutate(id_nb = paste(nx, ny, sep = "-"),
+         id_gear = paste(x, y, sep = "-")) %>% 
+  distinct(id_gear, id_nb)
+
+gears %>% 
+  left_join(groups, by = c("id_nb" = "id")) %>% 
+  group_by(id_gear) %>% 
+  mutate(n_gears = n_distinct(group_id)) %>% 
+  filter(n_gears > 1) %>% 
+  distinct(group_id) %>% 
+  left_join(groups) %>% 
+  group_by(id_gear, group_id) %>% 
+  summarise(part = paste(part, collapse = "") %>% parse_number()) %>% 
+  group_by(id_gear) %>% 
+  summarise(ratio = prod(part)) %>% 
+  summarise(sum(ratio))
