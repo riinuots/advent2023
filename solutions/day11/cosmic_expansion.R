8e8c72077b9fd229fe451d7ba07d1a2e19c5e403
riinuots <riinu.pius@ed.ac.uk>
2023-12-11 18:43:51 +0000

    fixed broken refactoring

--- solutions/day11/cosmic_expansion.R
+++ solutions/day11/cosmic_expansion.R
@@ -3,13 +3,17 @@ library(tidyverse)
 input_orig = read_lines("solutions/day11/input")
 nrows = length(input_orig)
 
-galaxies = input_orig %>% 
+input_matrix = input_orig %>% 
   map(~str_split_1(.x, "")) %>% 
   unlist() %>% 
-  matrix(nrow = nrows, byrow = TRUE) %>% 
-  which(input == "#", arr.ind = TRUE) %>% 
+  matrix(nrow = nrows, byrow = TRUE)
+
+galaxies = which(input_matrix == "#", arr.ind = TRUE) %>% 
   as_tibble() %>% 
   rowid_to_column("id")
+
+empty_rows = which(rowSums(input_matrix == "#") == 0)
+empty_cols = which(colSums(input_matrix == "#") == 0)
 
 # Part I
 wrangle_add = function(n_add = 0){
